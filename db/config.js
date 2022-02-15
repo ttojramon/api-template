55d97d4d1ea4a1d852980b43b66b3da04ad06452
ttojramon <tto.jramon.al@gmail.com>
2022-02-15 11:33:37 -0700

    add config db

---
+++ db/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    host: '',
+    database: '',
+    user: '',
+    port: '',
+    password: '',
+    uri: ''
+  },
+  test: {
+    host: '',
+    database: '',
+    user: '',
+    port: '',
+    password: '',
+    uri: ''
+  },
+  production: {
+    host: '',
+    database: '',
+    user: '',
+    port: '',
+    password: '',
+    uri: ''
+  }
+}
